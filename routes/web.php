d0d1454df5465e179fe2255197e5d018a269f7a6
Anugrah Bimantara <90950617+bimantaraz@users.noreply.github.com>
2023-09-20 15:26:01 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\mahasiswaConttroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+Route::resource('mahasiswa', mahasiswaConttroller::class);
